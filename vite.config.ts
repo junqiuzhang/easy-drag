451d94e6c80e5fef60b61be9362124ac75025998
zhangjunqiu <zhangjunqiu@stu.ouc.edu.cn>
2021-10-13 19:40:15 +0800

    feat(all): init

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import path from "path";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "draggable",
+    }
+  },
+});
